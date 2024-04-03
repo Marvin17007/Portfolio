aa2bed854be08c4f9f96cff73e50c7098132f0e8
AKARO JEUX Marvin <akarojeuxmarvin@gmail.comgit config --global user.email akarojeuxmarvin@gmail.com>
2024-04-03 21:51:36 +0200

    first

---
+++ js/body.js
@@ -0,0 +1,63 @@
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    e.target.matches('.next') && slider.append(items[0])
+    e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener('click', activate, false);
+
+function scrollFooter(scrollY, heightFooter) {
+    console.log(scrollY);
+    console.log(heightFooter);
+
+    if (scrollY >= heightFooter) {
+        $('footer').css({
+            'bottom': '0px'
+        });
+    }
+    else {
+        $('footer').css({
+            'bottom': '-' + heightFooter + 'px'
+        });
+    }
+}
+
+$(window).load(function () {
+    var windowHeight = $(window).height(),
+        footerHeight = $('footer').height(),
+        heightDocument = (windowHeight) + ($('.content').height()) + ($('footer').height()) - 20;
+
+    // Definindo o tamanho do elemento pra animar
+    $('#scroll-animate, #scroll-animate-main').css({
+        'height': heightDocument + 'px'
+    });
+
+    // Definindo o tamanho dos elementos header e conteúdo
+    $('header').css({
+        'height': windowHeight + 'px',
+        'line-height': windowHeight + 'px'
+    });
+
+    $('.wrapper-parallax').css({
+        'margin-top': windowHeight + 'px'
+    });
+
+    scrollFooter(window.scrollY, footerHeight);
+
+    // ao dar rolagem
+    window.onscroll = function () {
+        var scroll = window.scrollY;
+
+        $('#scroll-animate-main').css({
+            'top': '-' + scroll + 'px'
+        });
+
+        $('header').css({
+            'background-position-y': 50 - (scroll * 100 / heightDocument) + '%'
+        });
+
+        scrollFooter(scroll, footerHeight);
+    }
+});
